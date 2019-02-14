fb42ad75414834ee6cd7696161f4d48619a0668a
Herbert Mühlburger <herbert.muehlburger@bearingpoint.com>
2019-02-14 13:56:51 +0100

    update code

--- TEx-03/code/web-server/main.go
+++ TEx-03/code/web-server/main.go
@@ -3,33 +3,35 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
-type Movie struct {
+type movie struct {
 	Title  string
 	Year   int `json:"released"`
 	Actors []string
 }
 
-var movies = []Movie{
+var movies = []movie{
 	{Title: "The Matrix", Year: 1996, Actors: []string{"Keanu Reeves", "Carrie Moss"}},
 	{Title: "Matrix Reloaded", Year: 2003, Actors: []string{"Monica Bellucci", "Keanu Reeves"}},
 	{Title: "The Rock", Year: 1999, Actors: []string{"Sean Connery", "Niclas Cage"}},
 }
 
 func main() {
-	http.HandleFunc("/", path)
-	http.HandleFunc("/movies", getMovies)
-	http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/movies", movieHandler)
+
+	fmt.Printf("The web-server is live at http://%s.", "127.0.0.1:3000")
+	log.Fatal(http.ListenAndServe("127.0.0.1:3000", nil))
 }
 
-// handler echoes the Path component of the requested URL.
-func path(w http.ResponseWriter, r *http.Request) {
+func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
 
-func getMovies(w http.ResponseWriter, r *http.Request) {
+func movieHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(movies)
 }
